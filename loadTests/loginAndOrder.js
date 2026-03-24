ffe619b68636e1c165d0c8e4e70bceab789a84f5
jakenef <jakenef@byu.edu>
2026-03-24 16:59:08 -0600

    feat: add load testing script for login and order scenarios

---
+++ loadTests/loginAndOrder.js
@@ -0,0 +1,213 @@
+import { sleep, check, group, fail } from "k6";
+import http from "k6/http";
+import jsonpath from "https://jslib.k6.io/jsonpath/1.0.2/index.js";
+
+export const options = {
+  cloud: {
+    distribution: {
+      "amazon:us:ashburn": { loadZone: "amazon:us:ashburn", percent: 100 },
+    },
+    apm: [],
+  },
+  thresholds: {},
+  scenarios: {
+    Scenario_1: {
+      executor: "ramping-vus",
+      gracefulStop: "30s",
+      stages: [
+        { target: 5, duration: "30s" },
+        { target: 15, duration: "1m" },
+        { target: 10, duration: "30s" },
+        { target: 0, duration: "30s" },
+      ],
+      gracefulRampDown: "30s",
+      exec: "scenario_1",
+    },
+  },
+};
+
+export function scenario_1() {
+  let response;
+
+  const vars = {};
+
+  group("page_2 - https://pizza.jakenef.click/", function () {
+    response = http.get("https://pizza.jakenef.click/", {
+      headers: {
+        accept:
+          "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+        "accept-encoding": "gzip, deflate, br, zstd",
+        "accept-language": "en-US,en;q=0.9",
+        "cache-control": "max-age=0",
+        "if-modified-since": "Thu, 19 Mar 2026 19:02:58 GMT",
+        "if-none-match": '"0abcd5c9247856dc575b7c1ca3c15ed7"',
+        priority: "u=0, i",
+        "sec-ch-ua":
+          '"Chromium";v="146", "Not-A.Brand";v="24", "Google Chrome";v="146"',
+        "sec-ch-ua-mobile": "?1",
+        "sec-ch-ua-platform": '"Android"',
+        "sec-fetch-dest": "document",
+        "sec-fetch-mode": "navigate",
+        "sec-fetch-site": "same-origin",
+        "sec-fetch-user": "?1",
+        "upgrade-insecure-requests": "1",
+      },
+    });
+    sleep(113);
+
+    response = http.put(
+      "https://pizza-service.jakenef.click/api/auth",
+      '{"email":"jakenef@byu.edu","password":"password"}',
+      {
+        headers: {
+          accept: "*/*",
+          "accept-encoding": "gzip, deflate, br, zstd",
+          "accept-language": "en-US,en;q=0.9",
+          "content-type": "application/json",
+          origin: "https://pizza.jakenef.click",
+          priority: "u=1, i",
+          "sec-ch-ua":
+            '"Chromium";v="146", "Not-A.Brand";v="24", "Google Chrome";v="146"',
+          "sec-ch-ua-mobile": "?1",
+          "sec-ch-ua-platform": '"Android"',
+          "sec-fetch-dest": "empty",
+          "sec-fetch-mode": "cors",
+          "sec-fetch-site": "same-site",
+        },
+      },
+    );
+    if (
+      !check(response, {
+        "status equals 200": (response) => response.status.toString() === "200",
+      })
+    ) {
+      console.log(response.body);
+      fail("Login was *not* 200");
+    }
+
+    vars["token1"] = jsonpath.query(response.json(), "$.token")[0];
+
+    sleep(10);
+
+    response = http.get("https://pizza-service.jakenef.click/api/order/menu", {
+      headers: {
+        accept: "*/*",
+        "accept-encoding": "gzip, deflate, br, zstd",
+        "accept-language": "en-US,en;q=0.9",
+        authorization: `Bearer ${vars["token1"]}`,
+        "content-type": "application/json",
+        origin: "https://pizza.jakenef.click",
+        priority: "u=1, i",
+        "sec-ch-ua":
+          '"Chromium";v="146", "Not-A.Brand";v="24", "Google Chrome";v="146"',
+        "sec-ch-ua-mobile": "?1",
+        "sec-ch-ua-platform": '"Android"',
+        "sec-fetch-dest": "empty",
+        "sec-fetch-mode": "cors",
+        "sec-fetch-site": "same-site",
+      },
+    });
+
+    response = http.get(
+      "https://pizza-service.jakenef.click/api/franchise?page=0&limit=20&name=*",
+      {
+        headers: {
+          accept: "*/*",
+          "accept-encoding": "gzip, deflate, br, zstd",
+          "accept-language": "en-US,en;q=0.9",
+          authorization: `Bearer ${vars["token1"]}`,
+          "content-type": "application/json",
+          origin: "https://pizza.jakenef.click",
+          priority: "u=1, i",
+          "sec-ch-ua":
+            '"Chromium";v="146", "Not-A.Brand";v="24", "Google Chrome";v="146"',
+          "sec-ch-ua-mobile": "?1",
+          "sec-ch-ua-platform": '"Android"',
+          "sec-fetch-dest": "empty",
+          "sec-fetch-mode": "cors",
+          "sec-fetch-site": "same-site",
+        },
+      },
+    );
+    sleep(20.7);
+
+    response = http.get("https://pizza-service.jakenef.click/api/user/me", {
+      headers: {
+        accept: "*/*",
+        "accept-encoding": "gzip, deflate, br, zstd",
+        "accept-language": "en-US,en;q=0.9",
+        authorization: `Bearer ${vars["token1"]}`,
+        "content-type": "application/json",
+        origin: "https://pizza.jakenef.click",
+        priority: "u=1, i",
+        "sec-ch-ua":
+          '"Chromium";v="146", "Not-A.Brand";v="24", "Google Chrome";v="146"',
+        "sec-ch-ua-mobile": "?1",
+        "sec-ch-ua-platform": '"Android"',
+        "sec-fetch-dest": "empty",
+        "sec-fetch-mode": "cors",
+        "sec-fetch-site": "same-site",
+      },
+    });
+    sleep(9.2);
+
+    response = http.post(
+      "https://pizza-service.jakenef.click/api/order",
+      '{"items":[{"menuId":2,"description":"Pepperoni","price":0.0042},{"menuId":1,"description":"Veggie","price":0.0038},{"menuId":3,"description":"Margarita","price":0.0042}],"storeId":"1","franchiseId":1}',
+      {
+        headers: {
+          accept: "*/*",
+          "accept-encoding": "gzip, deflate, br, zstd",
+          "accept-language": "en-US,en;q=0.9",
+          authorization: `Bearer ${vars["token1"]}`,
+          "content-type": "application/json",
+          origin: "https://pizza.jakenef.click",
+          priority: "u=1, i",
+          "sec-ch-ua":
+            '"Chromium";v="146", "Not-A.Brand";v="24", "Google Chrome";v="146"',
+          "sec-ch-ua-mobile": "?1",
+          "sec-ch-ua-platform": '"Android"',
+          "sec-fetch-dest": "empty",
+          "sec-fetch-mode": "cors",
+          "sec-fetch-site": "same-site",
+        },
+      },
+    );
+    if (
+      !check(response, {
+        "status equals 200": (response) => response.status.toString() === "200",
+      })
+    ) {
+      console.log(response.body);
+      fail("Order req was *not* 200");
+    }
+
+    vars["pizzaJwt"] = jsonpath.query(response.json(), "$.jwt")[0];
+
+    sleep(4.9);
+
+    response = http.post(
+      "https://pizza-factory.cs329.click/api/order/verify",
+      `{"jwt":"${vars["pizzaJwt"]}"}`,
+      {
+        headers: {
+          accept: "*/*",
+          "accept-encoding": "gzip, deflate, br, zstd",
+          "accept-language": "en-US,en;q=0.9",
+          authorization: `Bearer ${vars["token1"]}`,
+          "content-type": "application/json",
+          origin: "https://pizza.jakenef.click",
+          priority: "u=1, i",
+          "sec-ch-ua":
+            '"Chromium";v="146", "Not-A.Brand";v="24", "Google Chrome";v="146"',
+          "sec-ch-ua-mobile": "?1",
+          "sec-ch-ua-platform": '"Android"',
+          "sec-fetch-dest": "empty",
+          "sec-fetch-mode": "cors",
+          "sec-fetch-site": "cross-site",
+          "sec-fetch-storage-access": "active",
+        },
+      },
+    );
+  });
+}
